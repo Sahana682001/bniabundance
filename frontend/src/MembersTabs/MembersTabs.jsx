2eca8b89804f30a86fb5b83205de27ffc19610aa
Sahana682001 <sahana.digiworq6@gmail.com>
2025-07-01 15:11:20 +0530

    Update MembersTabs.jsx

--- frontend/src/MembersTabs/MembersTabs.jsx
+++ frontend/src/MembersTabs/MembersTabs.jsx
@@ -53,7 +53,7 @@ const MemberList = () => {
   useEffect(() => {
     const fetchMembers = async () => {
       try {
-        const response = await fetch("http://localhost:5000/api/members");
+        const response = await fetch("https://bniabundance-backend-0iqp.onrender.com/api/members");
         const data = await response.json();
         setMembers(data);
       } catch (err) {
