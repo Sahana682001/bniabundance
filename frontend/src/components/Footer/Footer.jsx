4a9c6ec2a8766d2907b84c6a2a5fad31cbdcfd1c
Sahana682001 <sahana.digiworq6@gmail.com>
2025-07-03 10:00:09 +0530

    Update Footer.jsx

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -4,16 +4,23 @@ import './Footer.css';
 
 const Footer = () => {
   const location = useLocation();
-  const hideFooterRoutes = ['/login', '/banner']; // Routes to hide footer
+  const hideFooterRoutes = ['/login', '/banner'];
   if (hideFooterRoutes.includes(location.pathname)) return null;
 
-  const fullWidthRoutes = ['/dashboard'];
-  const isFullWidth = fullWidthRoutes.includes(location.pathname);
+  const isFullWidth = location.pathname === '/dashboard';
+  const footerStyle = isFullWidth
+    ? {} // no position or top
+    : { position: 'absolute', top: '1920px' };
 
   return (
-    <footer className={`custom-footer ${isFullWidth ? 'full-width' : ''}`}>
+    <footer className={`custom-footer ${isFullWidth ? 'full-width' : ''}`} style={footerStyle}>
       <p>
-        © 2025 <strong>bni-india.in</strong> All rights reserved. Designed & Developed by <strong><a style={{ color: "white" }} href='https://www.digiworq.com/' target='_blank' rel="noreferrer">DIGIWORQ</a></strong>
+        © 2025 <strong>bni-india.in</strong> All rights reserved. Designed & Developed by{' '}
+        <strong>
+          <a style={{ color: 'white' }} href='https://www.digiworq.com/' target='_blank' rel='noreferrer'>
+            DIGIWORQ
+          </a>
+        </strong>
       </p>
     </footer>
   );
